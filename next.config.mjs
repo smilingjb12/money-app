ed55bf85256e4fa26552efb3bcfafcdbb450841b
smilingjb12 <smilingjb12@gmail.com>
2025-03-11 23:57:02 +0100

    Fixing CI errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: process.env.NEXT_PUBLIC_CONVEX_URL.replace('https://', '') }],
+    remotePatterns: [{
+      hostname: process.env.NEXT_PUBLIC_CONVEX_URL?.replace('https://', '') || 'convex.dev'
+    }],
   },
 };
 
